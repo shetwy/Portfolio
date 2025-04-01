bec9b4380e55a3f8a99ef1cd76a1b7b0da70e793
SherryTorrefranca <sherry.torrefranca2004@gmail.com>
2025-04-01 21:12:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
